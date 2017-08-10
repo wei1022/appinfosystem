b61daea866ba4f8b4709e7b193690c63a5c949c2
wei1022 <1767818889@qq.com>
2017-08-10 22:50:16 +0800

    AppInfoSystem branch src

---
+++ src/pojo/BackendUser.java
@@ -0,0 +1,81 @@
+package cn.appinfo.pojo;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class BackendUser {
+	private Integer id;//编号
+	private String userCode;//编码
+	private String userName;//名字
+	private String userPassword;//密码
+	private Integer userType;//用户类型编号
+	private Integer createdBy;//创建者
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date creationDate;//创建时间
+	private Integer modifyBy;//修改者
+	private Date modifyDate;//修改时间
+	private String userTypeName;//用户类型名字
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public Integer getUserType() {
+		return userType;
+	}
+	public void setUserType(Integer userType) {
+		this.userType = userType;
+	}
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public Integer getModifyBy() {
+		return modifyBy;
+	}
+	public void setModifyBy(Integer modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public String getUserTypeName() {
+		return userTypeName;
+	}
+	public void setUserTypeName(String userTypeName) {
+		this.userTypeName = userTypeName;
+	}
+	
+	
+}
